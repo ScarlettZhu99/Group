c25288cf12795242f5c7460ba1ce56dfc6894db3
flyshuo <slu11@stevens.edu>
2018-04-07 16:46:04 -0400

    Shuo Lu

--- Postscript.h
+++ Postscript.h
@@ -78,5 +78,42 @@ public:
         s.write(buffer, size);
     }
 
-  
+    void drawCircle(double x, double y, double r){
+        char buffer[256];
+        int size = sprintf(buffer, "%f %f %f 0 360 arc\n%sstroke\n", x, y, r, strokeColor);
+        s.write(buffer, size);
+    }
+
+    void fillCircle(double x, double y, double r){
+        char buffer[256];
+        int size = sprintf(buffer, "%f %f %f 0 360 arc\n%sfill\n", x, y, r, fillColor);
+        s.write(buffer, size);
+    }
+
+    void text(double x, double y, const char* str){
+        unsigned int len = strlen(str);
+        char buffer[256 + len];
+        int size = sprintf(buffer, "/Times-Roman findfont 18 scalefont setfont newpath %f %f moveto (%s) %sshow\n", x, y, str, strokeColor);
+        s.write(buffer, size);
+    }
+
+    void grid(double x1, double interval_x, double x2, double y1, double interval_y, double y2){
+        char buffer1[256];
+        char buffer2[256];
+        char buffer3[256];
+        char buffer4[256];
+
+        int bins_x = int((x2 - x1) / interval_x);
+        int bins_y =int((y2 - y1) / interval_y);
+
+        int size1 = sprintf(buffer1, "/vline {%f %f moveto %f %f lineto stroke} def\n", x1, y1, x1, y2);
+        s.write(buffer1, size1);
+        int size2 = sprintf(buffer2, "gsave\nvline\n%d {%f 0 translate vline} repeat\ngrestore\n", bins_x, interval_x);
+        s.write(buffer2, size2);
+
+        int size3 = sprintf(buffer3, "/hline {%f %f moveto %f %f lineto stroke} def\n", x1, y1, x2, y1);
+        s.write(buffer3, size3);
+        int size4 = sprintf(buffer4, "gsave\nhline\n%d {0 %f translate hline} repeat\ngrestore\n", bins_y, interval_y);
+        s.write(buffer4, size4);
+    }
 };
